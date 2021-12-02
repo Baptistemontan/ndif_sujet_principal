90f23f7cbdac509e0539183a6fe25fcd2642ab9c
Bamontan <baptiste.demontangon@gmail.com>
2021-12-02 22:52:39 +0100

    locale choice in footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -4,11 +4,29 @@ import Link from "next/link";
 import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa";
 import { handleEnterKeyDown, openNewTab } from "@utils";
 
+import i18nConfig from "@i18n";
+import { useRouter } from "next/dist/client/router";
+
+const { locales } = i18nConfig;
+
 export default function Footer() {
   const { t } = useTranslation();
+  const router = useRouter();
   return (
     <footer className={Style.wrapper}>
-      <div className={Style.content}>footer</div>
+      <div className={Style.content}>
+        <div className={Style.locales}>
+          <ul>
+            {locales.map((locale) => (
+              <li key={locale}>
+                <Link href={router.asPath} locale={locale}>
+                  {locale}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
