fba9146b3847c2bdc9aa33778da6e55259f7c697
Bamontan <baptiste.demontangon@gmail.com>
2021-12-02 22:56:32 +0100

    init home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Style from "@styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={Style.container} onClick={() => console.log("test")}>
       <Head>
         <title>Home</title>
       </Head>
