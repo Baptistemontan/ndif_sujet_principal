86b9f32e017a4188288dd854bfe974741b383368
Bamontan <baptiste.demontangon@gmail.com>
2021-12-02 22:56:27 +0100

    added server url

--- utils/envconst.ts
+++ utils/envconst.ts
@@ -1,3 +1,5 @@
-export const dev: boolean = process.env.NODE_ENV !== "production";
+export const dev = process.env.NODE_ENV !== "production";
 
-export const API_URL: string = "?";
+export const API_URL = "?";
+
+export const serverURL = dev ? "http://localhost:3000" : "?";
