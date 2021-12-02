e04305d8e7f72dbea2b6156231cd42e723cb28ff
Bamontan <baptiste.demontangon@gmail.com>
2021-12-02 18:14:20 +0100

    configuration of next translate plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const nextTranslate = require("next-translate");
+
+const date = new Date();
+
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_APP_BUILD_TIME: date.toString(),
+    NEXT_PUBLIC_APP_BUILD_TIMESTAMP: +date,
+  },
   reactStrictMode: true,
-}
+};
+
+// module.exports = {
+//   ...nextTranslate(),
+// };
+
+
+
+module.exports = withPlugins([nextTranslate], nextConfig);
