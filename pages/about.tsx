aa79362103b60cefaeb5b7debaa7315dcdbd4a43
Axetriks <92303052+Axetriks@users.noreply.github.com>
2021-12-03 05:06:25 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,21 @@
 export default function About() {
   return <></>;
-  <div>
-    <p>
+  <header>
+    <h1<
       Nuit de l'info  LesStmgDeLESIEA
-    <p>
-  <div>
+    <h1>
+  <header
 
-  <div>
-    <p>
-      Défi principal de l'évènement "La Nuit De L'Info", ce site à été créé afin de pouvoir rechercher des informations sur le site "sauveteurdudunkerquois.fr".
-    <p>
-    <p>
-      Nous avons donc créé une base de donnée regroupant les informations des sauveteurs, des bateaux, et des sauvetages référencés sur le site. Quand une recherche est effectuée, le site recherche les élèments correspondants dans cette base de données, et renvoie un lien vers les différents articles existants.
-    <p>
-      Par exemple, en recherchant le nom de famille d'un sauveteur, vous obtiendrez en résultat tout les sauvetages auxquels il a participé, ainsi que l'article sur ce sauveteur.
-    <p>
-  <div>
+  <body>
+    <h2>
+      <p>
+        Défi principal de l'évènement "La Nuit De L'Info", ce site à été créé afin de pouvoir rechercher des informations sur le site "sauveteurdudunkerquois.fr".
+      <p>
+      <p>
+        Nous avons donc créé une base de donnée regroupant les informations des sauveteurs, des bateaux, et des sauvetages référencés sur le site. Quand une recherche est effectuée, le site recherche les élèments correspondants dans cette base de données, et renvoie un lien vers les différents articles existants.
+      <p>
+        Par exemple, en recherchant le nom de famille d'un sauveteur, vous obtiendrez en résultat tout les sauvetages auxquels il a participé, ainsi que l'article sur ce sauveteur.
+      <p>
+    <h2>
+  <body>
 }
