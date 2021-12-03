9b8be8b96b8d84cf113d05a2d5ac82ff645fb9ce
Martin <47635335+Martiiiiing@users.noreply.github.com>
2021-12-03 01:15:46 +0100

    types.ts update

--- utils/types.ts
+++ utils/types.ts
@@ -37,7 +37,60 @@ export interface ISavior {
   name: string;
   first_name: string;
   role: string;
+  naissance: string;
+  mort: Date;
+  lieu_naissance: string;
+  lieu_mort: string;
+  lieu_marriage: string;
+  conjoint: string;
 }
+
+export interface ISaviorFirestore {
+  id: string;
+  nom: string;
+  prenom: string;
+  role: string;
+  naissance: string;
+  mort: Date;
+  lieu_naissance: string;
+  lieu_mort: string;
+  lieu_marriage: string;
+  conjoint: string;
+}
+export const saviorConverter = {
+  toFirestore: (savior: ISavior) => {
+    return {
+      name: savior.name,
+      first_name: savior.first_name,
+      role: savior.role,
+      naissance: savior.naissance,
+      mort: savior.mort,
+      lieu_naissance: savior.lieu_naissance,
+      lieu_mort: savior.lieu_mort,
+      lieu_marriage: savior.lieu_marriage,
+      conjoint: savior.conjoint
+    };
+  },
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot<ISaviorFirestore>,
+    options?: SnapshotOptions,
+  ): ISavior => {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      name: data.nom,
+      first_name: data.prenom,
+      role: data.role,
+      naissance: data.naissance,
+      mort: data.mort,
+      lieu_naissance: data.lieu_naissance,
+      lieu_mort: data.lieu_mort,
+      lieu_marriage: data.lieu_marriage,
+      conjoint: data.conjoint
+    };
+  },
+};
+
 
 export interface ISauvetage {
   id: string;
@@ -49,6 +102,45 @@ export interface ISauvetage {
   nb_saved: number;
   saved: boolean;
 }
+export interface ISauvetageFirestore {
+  id: string;
+  bateau: IBoatFirestore;
+  sauveteur: ISaviorFirestore;
+  date: string;
+  lieu: string;
+  nb_morts: number;
+  nb_sauves: number;
+  sauve: boolean;
+}
+export const sauvetageConverter = {
+  toFirestore: (sauvetage: ISauvetage) => {
+    return {
+      bateau: sauvetage.boat,
+      date: sauvetage.date,
+      lieu: sauvetage.location,
+      nb_morts: sauvetage.nb_dead,
+      nb_sauves: sauvetage.nb_saved,
+      sauve: sauvetage.saved,
+      sauveteur: sauvetage.savior,
+    };
+  },
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot<ISauvetageFirestore>,
+    options?: SnapshotOptions,
+  ): ISauvetage => {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      boat: data.bateau,
+      date: data.date,
+      location: data.lieu,
+      nb_dead: data.nb_morts,
+      nb_saved: data.nb_sauves,
+      saved: data.sauve,
+      savior: data.sauveteur,
+    };
+  },
+};
 
 // TgfeRlnvbkO9n72kD94O bato
 // zL1pzx7aYfz5AJdQcLHY sauvetor
