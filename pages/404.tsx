91a751dfff3a8447a8646d64bf8150a4d35402d0
Bamontan <baptiste.demontangon@gmail.com>
2021-12-03 01:51:48 +0100

    not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,16 @@
-export default function Page404() {
+import Style from "@styles/404.module.scss";
+import useTranslation from "next-translate/useTranslation";
+
+export default function NotFound() {
+  const { t } = useTranslation();
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className={Style.wrapper}>
+      <div className={Style.content}>
+        <div className={Style.number}>
+          <h1>404</h1>
+        </div>
+        <h2>{t("common:404")}</h2>
+      </div>
     </div>
   );
 }
